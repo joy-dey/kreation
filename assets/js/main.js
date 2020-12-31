24f4ef3df60ce415ed69972d3e2978d9f631342f
RahulBhadra <67007397+RahulBhadra@users.noreply.github.com>
2020-12-31 12:34:18 +0530

    popup change

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,28 +15,28 @@ let searchbar = document.querySelector('#search'),
     loadingPage = document.querySelector('#splash-screen'),
     backButton = document.querySelector('#goback');
 
-// if (searchbar) {
-//     services.forEach(service => {
-//         servicesAutoComplete.innerHTML += `<option>${service}</option>`;
-//     })
+if (searchbar) {
+    services.forEach(service => {
+        servicesAutoComplete.innerHTML += `<option>${service}</option>`;
+    })
 
-//     searchbar.addEventListener('keydown', e => {
-//         if (e.key === "Enter") {
-//             searchResults.innerHTML = `
-//             <div class="row my-4">
-//                 <div class="col-12">
-//                     <div class="search-card p-4 d-flex" style="background-image: url('assets/img/facial.jpg');">
-//                         <div class="overlay-text text-left d-flex flex-column justify-content-end">
-//                             <h2>${searchbar.value}</h2>
-//                             <p class="h6 mb-0">₹3600 - onwards</p>
-//                         </div>
-//                      </div>
-//                 </div>
-//             </div>
-//             `
-//         }
-//     })
-// }
+    searchbar.addEventListener('keydown', e => {
+        if (e.key === "Enter") {
+            searchResults.innerHTML = `
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="search-card p-4 d-flex" style="background-image: url('assets/img/facial.jpg');">
+                        <div class="overlay-text text-left d-flex flex-column justify-content-end">
+                            <h2>${searchbar.value}</h2>
+                            <p class="h6 mb-0">₹3600 - onwards</p>
+                        </div>
+                     </div>
+                </div>
+            </div>
+            `
+        }
+    })
+}
 setTimeout(() => {
     if (loadingPage) {
         window.location.href = 'landing.html';
